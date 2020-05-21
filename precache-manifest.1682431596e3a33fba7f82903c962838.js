f6c138d11989a57601d4bdd14f2aece60470f699
Ariatar <hellgate9292@gmail.com>
2020-05-21 12:03:08 +0300

    sic

---
+++ precache-manifest.1682431596e3a33fba7f82903c962838.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53beea27ab8550db555c",
+    "url": "/map/css/app.48ebe494.css"
+  },
+  {
+    "revision": "fc66c05b6ef94e9a65a5",
+    "url": "/map/css/chunk-vendors.4d1c7d25.css"
+  },
+  {
+    "revision": "47d7448748dbd4c4144536d5313c4acf",
+    "url": "/map/img/Screenshot_1.47d74487.png"
+  },
+  {
+    "revision": "82f08526fa2da7b0a0af455be1eebb8a",
+    "url": "/map/index.html"
+  },
+  {
+    "revision": "53beea27ab8550db555c",
+    "url": "/map/js/app.583764c0.js"
+  },
+  {
+    "revision": "fc66c05b6ef94e9a65a5",
+    "url": "/map/js/chunk-vendors.08e563c6.js"
+  },
+  {
+    "revision": "0768a3192a63b3f0a43923aefe8eb445",
+    "url": "/map/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/map/robots.txt"
+  }
+]);
